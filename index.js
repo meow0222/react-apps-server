68a2eb8edf6dee9ea3b847fa99a2ff273369cf71
meow0222 <meow222.csgo@gmail.com>
2023-08-25 11:58:41 +0900

    Fix some errors and organize format

--- index.js
+++ index.js
@@ -45,90 +45,6 @@ app.options('/', (req,res) => {
     res.sendStatus(200);
 });
 
-/*
-// GET for Login
-app.get('/login',textBodyParser, async function(req, res){
-    // Print the HTTP request headers
-    console.log('req.headers: ', req.headers);
-
-    const reqOrigin = req.headers['origin'];
-    const reqTask = req.headers['casino'];
-
-    console.log("Request from" + reqOrigin + "for route" + req.url + "with method " + req.method + "for task" + reqTask);
-
-    // TASK Check
-    if (reqTask === 'login') {
-        try {
-            const loginResult = await authenticateUser(req);
-            console.log('authenticateUser result: ', loginResult);
-            if(loginResult == true) {
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                res.setHeader('Access-Control-Expose-Headers', 'request-result');
-                res.setHeader('request-result', 'Request ' + req.method + ' was received successfully');
-                res.status(200).send("Login Successful");
-            } else {
-                res.status(403).send("Login Failed");
-            }
-        } catch(error) {
-            console.log("authenticateUser error: ", error);
-            res.status(500).send("Server Error");
-        }
-    }
-    res.end();
-});
-
-// GET for Blackjack
-app.get('/blackjack', async function(req, res) {
-    console.log('req.headers: ', req.headers);
-
-    const reqOrigin = req.headers['origin'];
-    const reqTask = req.headers['casino'];
-    console.log("Request from: " + reqOrigin + " for route: " + req.url + " with method: " + req.method + " for task: " + reqTask);
-
-    if (reqTask === 'blackjack') {
-        try {
-            const deck = createDeck();
-            const players = createPlayers(2);
-            console.log(deck);
-            
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Expose-Headers', 'request-result');
-            res.setHeader('request-result', 'Request ' + req.method + ' was received successfully');
-            res.status(200).json({ players, deck });
-            
-        } catch(error) {
-            console.log("authenticateUser error: ", error);
-            res.status(500).send("Server Error");
-        }
-    }
-
-});
-
-
-// POST for Signup
-app.post('/login', async function(req, res) {
-    console.log('req.headers: ', req.headers);
-
-    const reqOrigin = req.headers['origin'];
-    const reqTask = req.headers['casino'];
-    const reqBody = req.body;
-
-    console.log("Request from" + reqOrigin + "for route" + req.url + "with method " + req.method + "for task" + reqTask);
-    console.log(reqBody);
-
-    if (reqTask === 'signup') {
-        try {
-            const filePath = './data/users.json';
-            const username = reqBody.username;
-            const password = reqBody.password;
-            await addUser(filePath, username, password);
-        } catch(error) {
-            console.log("error: ", error);
-            res.status(500).send("Server Error");
-        }
-    }
-});
-*/
 
 // Listen
 app.listen(port, (err)=>{
@@ -139,39 +55,7 @@ app.listen(port, (err)=>{
     console.log(`Server listening on http://localhost:${port}`);
 });
 
-
-/*
-//get for slotmachine
-app.get('/api/todos', async function (req, res) {
-    // print the HTTP Request Headers
-    console.log('req.headers: ', req.headers); 
-
-    const reqOrigin = req.headers['origin']; // get the origin of the request
-    const reqTask = req.headers['task']; // get the task of the request
-
-    console.log("Processing request from " + reqOrigin + " for route " + req.url + " with method " + req.method + " for task: " + reqTask);
-    
-    if (reqTask === 'bet-coin') {
-        try {
-            const betClick = handleBetClick;
-            const reward = calculateReward;
-            console.log("handleBetClick returned betClick: ", betClick);
-            console.log("calculateRward returned betClick: ", reward);
-
-
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Expose-Headers', 'request-result');
-            res.setHeader('request-result', 'Request ' + req.method + ' was received successfully');
-            res.status(200);
-            
-        } catch(error) {
-            console.log("authenticateUser error: ", error);
-            res.status(500).send("Server Error");
-        }
-    }
-
-});
-*/
+// ----------------------------------- TODOs -------------------------------------------
 
 app.get('/api/todos', async (req, res) => {
     try {
@@ -215,6 +99,7 @@ app.get('/api/todos', async (req, res) => {
       res.status(500).json({ error: 'Error adding new todo' });
     }
   });
+// ------------------------------------ END --------------------------------------------
 
 
 //maya
